f86f8e682c7e14d2a47677a8ebb7e59d49cf8d04
louisdizon <louis.dizon@gmail.com>
2020-07-06 14:46:20 +0800

    Modified global font and added main.css for additional styling

--- src/main/java/com/falcon/orders/OrderController.java
+++ src/main/java/com/falcon/orders/OrderController.java
@@ -1,5 +1,14 @@
 package com.falcon.orders;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class OrderController {
 
+	@GetMapping("/orders")
+    public String dashboard(Model model) {
+        return "order/order";
+    }
 }
