ecc75b8cddd4a44c82736843a4838a864eeeb28e
Cj Pangilinan <cjpangilinan@gmail.com>
2020-08-01 23:22:18 -0400

    Add UserActivity entity. Fix data.sql for auto insert.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,14 @@
-insert into user(id,username,password,email,first_name,last_name) values
-  (1,'admin','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjp@tuta.io','Sidyey','Pi');
-insert into role(id,name) values (1,'ROLE_ADMIN');
-insert into users_roles(user_id,role_id) values(1,1);
+insert ignore into user(id,username,password,email,first_name,last_name) values
+  (1,'admin','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjp@tuta.io','Sidyey','Pi'),
+  (2,'user','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjp@tutanota.de','LA','Lang');
+insert ignore into role(id,name) values 
+  (1,'ROLE_ADMIN'),(2,'ROLE_USER');
+insert ignore into users_roles(user_id,role_id) values (1,1),(2,2);
 
-insert into product_category(id,name) values (1,'Body and Paint');
-insert into product(id,category_id,name,brand,for_vehicle,color,aqui_price,srp,other_details,supplier_name,stock_level,threshold) values
-  (1,1,'Bumper','Brand X','Vios','Bronze Mica Metallic',2000,2500,'brand new','Secret Supplier',20,10)
+insert ignore into product_category(id,name) values (1,'Body and Paint');
+insert ignore into product(id,category_id,name,brand,for_vehicle,color,aqui_price,srp,other_details,supplier_name,stock_level,threshold) values
+  (1,1,'Bumper1','Brand X','Vios','Bronze Mica Metallic',2000,2500,'brand new','Secret Supplier',20,10),
+  (2,1,'Bumper2','Brand X','Vios','Bronze Mica Metallic',2000,2500,'brand new','Secret Supplier',20,10),
+  (3,1,'Bumper3','Brand X','Vios','Bronze Mica Metallic',2000,2500,'brand new','Secret Supplier',20,10),
+  (4,1,'Bumper4','Brand X','Vios','Bronze Mica Metallic',2000,2500,'brand new','Secret Supplier',20,10),
+  (5,1,'Bumper5','Brand X','Vios','Bronze Mica Metallic',2000,2500,'brand new','Secret Supplier',20,10);
