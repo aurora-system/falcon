74cdf154451c75324bce5cab3e591b052adc5215
Cj Pangilinan <cjpangilinan@gmail.com>
2020-08-02 14:57:17 -0400

    updated data.sql for postgresql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,24 @@
-insert ignore into user(id,username,password,email,first_name,last_name) values
+insert into user(id,username,password,email,first_name,last_name) values
   (1,'admin','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjp@tuta.io','Sidyey','Pi'),
-  (2,'user','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjp@tutanota.de','LA','Lang');
-insert ignore into role(id,name) values 
-  (1,'ROLE_ADMIN'),(2,'ROLE_USER');
-insert ignore into users_roles(user_id,role_id) values (1,1),(2,2);
+  (2,'user','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjp@tutanota.de','LA','Lang')
+  on duplicate key ignore returning id;
+insert into role(id,name) values 
+  (1,'ROLE_ADMIN'),(2,'ROLE_USER')
+  on duplicate key ignore returning id;
+insert into users_roles(user_id,role_id) values (1,1),(2,2)
+  on duplicate key ignore returning id;
 
-insert ignore into product_category(id,name) values (1,'Body and Paint');
-insert ignore into product(id,category_id,name,brand,for_vehicle,color,aqui_price,srp,other_details,supplier_name,stock_level,threshold) values
+insert into product_category(id,name) values (1,'Body and Paint')
+  on duplicate key ignore returning id;
+insert into product(id,category_id,name,brand,for_vehicle,color,aqui_price,srp,other_details,supplier_name,stock_level,threshold) values
   (1,1,'Bumper1','Brand X','Vios','Bronze Mica Metallic',2000,2500,'brand new','Secret Supplier',20,10),
   (2,1,'Bumper2','Brand X','Vios','Bronze Mica Metallic',2000,2500,'brand new','Secret Supplier',20,10),
   (3,1,'Bumper3','Brand X','Vios','Bronze Mica Metallic',2000,2500,'brand new','Secret Supplier',20,10),
   (4,1,'Bumper4','Brand X','Vios','Bronze Mica Metallic',2000,2500,'brand new','Secret Supplier',20,10),
-  (5,1,'Bumper5','Brand X','Vios','Bronze Mica Metallic',2000,2500,'brand new','Secret Supplier',20,10);
+  (5,1,'Bumper5','Brand X','Vios','Bronze Mica Metallic',2000,2500,'brand new','Secret Supplier',20,10)
+  on duplicate key ignore returning id;
   
-insert ignore into in_out_history(id,trans_date,trans_type,product_id,product_count,updated_count) values
+insert into in_out_history(id,trans_date,trans_type,product_id,product_count,updated_count) values
   (1,'2020-07-01','IN',1,5,25),
-  (2,'2020-07-01','OUT',1,5,20);
+  (2,'2020-07-01','OUT',1,5,20)
+  on duplicate key ignore returning id;
