5f9d8d2d65e3e53df05597228b52af4cd9a1c8e4
Cj Pangilinan <cjpangilinan@gmail.com>
2020-08-02 15:27:31 -0400

    updated data.sql for postgresql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into user(id,username,password,email,first_name,last_name) values(1,'admin','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjp@tuta.io','Sidyey','Pi') on conflict (id) do nothing;
-insert into user(id,username,password,email,first_name,last_name) values(2,'user','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjp@tutanota.de','LA','Lang') on conflict (id) do nothing;
+insert into users(id,username,password,email,first_name,last_name) values(1,'admin','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjp@tuta.io','Sidyey','Pi') on conflict (id) do nothing;
+insert into users(id,username,password,email,first_name,last_name) values(2,'user','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjp@tutanota.de','LA','Lang') on conflict (id) do nothing;
 insert into role(id,name) values(1,'ROLE_ADMIN') on conflict (id) do nothing;
 insert into role(id,name) values(2,'ROLE_USER') on conflict (id) do nothing;
 insert into users_roles(user_id,role_id) values(1,1) on conflict (id) do nothing;
