cc632566144d8d97f0efb528ece2dbd5fe00e53c
Cj Pangilinan <cjpangilinan@gmail.com>
2020-08-02 15:29:26 -0400

    updated data.sql for postgresql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,8 @@ insert into users(id,username,password,email,first_name,last_name) values(1,'adm
 insert into users(id,username,password,email,first_name,last_name) values(2,'user','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjp@tutanota.de','LA','Lang') on conflict (id) do nothing;
 insert into role(id,name) values(1,'ROLE_ADMIN') on conflict (id) do nothing;
 insert into role(id,name) values(2,'ROLE_USER') on conflict (id) do nothing;
-insert into users_roles(user_id,role_id) values(1,1) on conflict (id) do nothing;
-insert into users_roles(user_id,role_id) values(2,2) on conflict (id) do nothing;
+insert into users_roles(user_id,role_id) values(1,1);
+insert into users_roles(user_id,role_id) values(2,2);
 
 insert into product_category(id,name) values(1,'Body and Paint') on conflict (id) do nothing;
 
