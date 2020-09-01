4fd5ef395d78557deaf8f2639f5be0a85c9210e2
Louis Dizon <louis.dizon@gmail.com>
2020-09-01 14:55:00 +0800

    Applied bootstrap on Product List page

--- src/main/java/com/falcon/product/Product.java
+++ src/main/java/com/falcon/product/Product.java
@@ -3,16 +3,19 @@ package com.falcon.product;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import com.falcon.orders.OrderItem;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.Data;
@@ -42,6 +45,10 @@ public class Product {
 	private String supplierName;
 	
 	@JsonManagedReference
+	@OneToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "id")
+	private OrderItem orderItem;
+	
 	@ManyToOne
 	@JoinColumn(name = "category_id")
 	ProductCategory category;
