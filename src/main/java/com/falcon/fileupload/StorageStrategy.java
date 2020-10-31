deb4fc05979d0665267b777f34f7fa69edfcb77b
Cj Pangilinan <cjpangilinan@gmail.com>
2020-10-31 13:16:11 -0400

    File upload function

---
+++ src/main/java/com/falcon/fileupload/StorageStrategy.java
@@ -0,0 +1,11 @@
+package com.falcon.fileupload;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageStrategy {
+    String[] uploadFile(MultipartFile multipartFile) throws Exception;
+    ResponseEntity<Object> downloadFile(String fileUrl, HttpServletRequest request) throws Exception;
+}
---
+++ src/main/java/com/falcon/firebase/FirebaseCredential.java
@@ -0,0 +1,16 @@
+package com.falcon.firebase;
+
+import lombok.Data;
+
+@Data public class FirebaseCredential {
+    private String type;
+    private String project_id;
+    private String private_key_id;
+    private String private_key;
+    private String client_email;
+    private String client_id;
+    private String auth_uri;
+    private String token_uri;
+    private String auth_provider_x509_cert_url;
+    private String client_x509_cert_url;
+}
