fa9b5f423dff291fb01c7b40a2ee42f34cc71f66
Cj Pangilinan <cjpangilinan@gmail.com>
2021-01-09 07:03:21 -0500

    Quick Search by keyword on Products and Sales Orders

--- src/main/java/com/falcon/salesorder/SalesOrderRepository.java
+++ src/main/java/com/falcon/salesorder/SalesOrderRepository.java
@@ -1,7 +1,12 @@
 package com.falcon.salesorder;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SalesOrderRepository extends CrudRepository<SalesOrder, Long>{
 
+	@Query("select o from SalesOrder o where o.invoiceNumber like %:keyword% OR o.remarks like %:keyword%")
+	List<SalesOrder> findAllByInvoiceNumberOrRemarks(String keyword);
 }
