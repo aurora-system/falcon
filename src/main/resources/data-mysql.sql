4ba04101a9ac27f5c73e53c9267b01d89f350f82
Cj Pangilinan <cjpangilinan@gmail.com>
2021-02-01 05:15:03 -0500

    Fixed initial users data sql

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -1,4 +1,4 @@
 insert ignore into users(id,username,password,email,first_name,last_name,role) values
   (1,'admin','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjp@tuta.io','Sidyey','Pi','ROLE_ADMIN'),
-  (2,'inventory','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjp@tutanota.de','LA','Lang','ROLE_INVENTORY')
+  (2,'inventory','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjp@tutanota.de','LA','Lang','ROLE_INVENTORY'),
   (3,'sales','$2y$11$6TP/e9ycvO4DwriSjfGip.6OVPNKuRArNyb4phwx9Kk3WPsdVjWUq','cjpxxx@tutanota.de','Sales','Sale','ROLE_SALES');
