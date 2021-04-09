08fadb804d9033a7bd56eeef0bd2dc77d5c94853
Louis Dizon <louis.dizon@gmail.com>
2021-04-09 20:53:15 +0800

    Help page initial

---
+++ src/main/java/com/falcon/common/HelpController.java
@@ -0,0 +1,16 @@
+package com.falcon.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelpController {
+
+	
+	@GetMapping("/help")
+	public String help(Model model) {
+	    
+	    return "help/help";
+	}
+}
